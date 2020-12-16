1bbd71c1a456896e32dc1610bd9ec14ca1da0060
Allison Mazurek <allison.mazurek5@gmail.com>
2020-12-16 13:25:58 -0500

    first commit hope to god my assignment files are private

---
+++ src/test/java/finalproject/SearchEngineTest.java
@@ -0,0 +1,34 @@
+package finalproject;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchEngineTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void crawlAndIndex() {
+    }
+
+    @Test
+    void assignPageRanks() {
+    }
+
+    @Test
+    void computeRanks() {
+    }
+
+    @Test
+    void getResults() {
+    }
+}
